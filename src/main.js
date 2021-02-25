7ed2db0b2497f2bde33121785efed7edf029c504
fangwenzheng <fangwenzheng88@163.com>
2021-02-25 14:56:18 +0800

    测试webhoook

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+// import store from './store';
 import './tools/element';
 import 'highlight.js/styles/github.css';
 import 'github-markdown-css/github-markdown.css';
 Vue.config.productionTip = false;
 new Vue({
   router,
-  store,
+  // store,
   render: (h) => h(App)
 }).$mount('#app');
