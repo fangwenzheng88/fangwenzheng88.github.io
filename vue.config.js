7149b23d9b4b2cd20b569a15a26eb08341ee7d5e
fangwenzheng <fangwenzheng88@163.com>
2021-04-22 16:46:06 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,24 @@
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = {
-  configureWebpack: {
-    externals: {
+  configureWebpack: (config) => {
+    config.externals = {
       vue: 'Vue',
       'vue-router': 'VueRouter',
       vuex: 'Vuex',
       axios: 'axios',
       'element-ui': 'ELEMENT',
       'highlight.js': 'hljs'
+    };
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new CompressionPlugin({
+          algorithm: 'gzip',
+          test: /\.(js|css||md)$/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false // 不删除源文件
+        })
+      );
     }
   }
   //chainWebpack: (config) => {
